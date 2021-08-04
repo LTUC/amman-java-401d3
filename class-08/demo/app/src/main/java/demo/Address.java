90e457c6939d2a80047cd4e79243b5c22f405ca8
maratouq <hanaamaratouk@gmail.com>
2021-08-04 15:58:38 +0300

    class 8 demo

--- class-08/demo/app/src/main/java/demo/Address.java
+++ class-08/demo/app/src/main/java/demo/Address.java
@@ -1,2 +1,19 @@
-package demo;public class Address {
+package demo;
+
+public class Address {
+    String streetAddress;
+    String city;
+    String state;
+    int postalCode;
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "streetAddress='" + streetAddress + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", postalCode=" + postalCode +
+                '}';
+    }
 }
+
--- class-08/demo/app/src/main/java/demo/Contact.java
+++ class-08/demo/app/src/main/java/demo/Contact.java
@@ -1,2 +1,24 @@
-package demo;public class Contact {
+package demo;
+
+public class Contact {
+    String firstName;
+    String lastName;
+    int age;
+    Address address;
+
+    public Contact(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", address=" + address +
+                '}';
+    }
 }
